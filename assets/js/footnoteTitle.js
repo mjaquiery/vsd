292a1b2d2cded879e03d05f2a154f20812701232
mjaquiery <m.jaquiery@sussex.ac.uk>
2019-08-02 16:05:03 +0100

    Footnote fix, suppress unique tags, CV fix

--- assets/js/footnoteTitle.js
+++ assets/js/footnoteTitle.js
@@ -2,7 +2,10 @@ function setFootnoteTitles() {
     const footnotes = document.querySelectorAll("sup a.footnote");
     footnotes.forEach(f => {
         const id = f.href.substr(f.href.search("#") + 1);
-        f.title = document.getElementById(id).innerText;
+        const title = document.getElementById(id).innerText;
+
+        // Drop the final return-to-text character for the title
+        f.title = title.substring(0, title.length - 2);
     });
 }
 
